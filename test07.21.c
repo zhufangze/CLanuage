d086d6b41fa3ca66dc2915bd37da7b5c5c0da568
zhufangze <123614170+zhufangze@users.noreply.github.com>
2023-07-22 00:26:33 +0800

    Add files via upload
    first day

---
+++ test07.21.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+//{
+//	int a, b,c,d;
+//	scanf_s("%d%d", &a, &b);
+//	c = a / b;
+//	d = a % b;
+//	printf("%d,%d\n", c, d);
+//	return 0;
+//
+//}
+//{
+//    int gift;
+//    scanf_s("%d", &gift);
+//    if (gift >= 140)
+//    {
+//        printf("Genius\n");
+//    }
+//    else
+//    {
+//        printf("Num\n");
+//    }
+//
+//
+//}
